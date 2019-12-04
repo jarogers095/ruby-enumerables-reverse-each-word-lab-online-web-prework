3dfe7b2e543a2e2b253d87f24a41236e4741af8b
James Rogers <jarogers095@gmail.com>
2019-12-04 03:05:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   reversed_sentence = ""
   
   split_sentence.each do |word|
-    reversed_sentence += word.reverse
+    reversed_sentence += (" " + word.reverse)
   end
   
   return reversed_sentence
